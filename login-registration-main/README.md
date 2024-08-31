70fd25e13cfad8115ab9d84083d9b98961d2d6cf
sowmya82001 <sowmya82001@gmail.com>
2024-08-31 07:53:23 +0530

    sample commit

---
+++ login-registration-main/README.md
@@ -0,0 +1,5 @@
+# ReactLoginSignupUITemplate
+
+Build React Login & Register UI Template with Bootstrap 5 - Create React authentication system such as login and sign up template using Bootstrap.
+
+[Build React Login & Register UI Template with Bootstrap 5](https://www.positronx.io/build-react-login-register-ui-template-with-bootstrap-4/)
---
+++ login-registration-main/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Login from "./components/login_component";
+import SignUp from "./components/signup_component";
+import UserDetails from "./components/userDetails";
+import Navbar from "./components/Navbar";
+import AdminHome from "./components/adminHome";
+import Product from "./components/products";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  const isLoggedIn = window.localStorage.getItem("loggedIn"); // Check if logged in
+  const userType = window.localStorage.getItem("userType");
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar isLoggedIn={isLoggedIn} userType={userType} />
+
+        <Routes>
+          {/* unauthorized route */}
+          {!isLoggedIn && (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/" element={<Login />} />
+            </>
+          )}
+
+          {/* ProtectedRoutes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/login" element={<Navigate to="/" />} />
+            <Route path="/register" element={<Navigate to="/" />} />
+            {userType != "Admin" ? (
+              <>
+                <Route path="/" element={<Navigate to="/userDetails" />} />
+                <Route path="/userDetails" element={<UserDetails />} />
+                <Route path="/products" element={<Product />} />
+                <Route path="/admin-dashboard" element={<Navigate to="/" />} />
+              </>
+            ) : (
+              <>
+                <Route path="/" element={<Navigate to="/admin-dashboard" />} />
+                <Route path="/userDetails" element={<Navigate to="/" />} />
+                <Route path="/products" element={<Navigate to="/" />} />
+                <Route path="/admin-dashboard" element={<AdminHome />} />
+              </>
+            )}
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
